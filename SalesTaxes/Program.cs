b50f89fd6be2dd3fbf8e064003c011930c87ad84
Alexandre Konioukhov <alexan1@gmail.com>
2019-11-23 17:31:38 -0500

    GetItemsList refactoring

--- SalesTaxes/Program.cs
+++ SalesTaxes/Program.cs
@@ -33,59 +33,79 @@ namespace SalesTaxes
 
         private static IEnumerable<ShoppingCartItem> GetItemsList(int listnum)
         {
-            return new List<ShoppingCartItem>
 
+            var lstItems = new List<ShoppingCartItem>();
+
+            switch (listnum)
             {
-                //input 1
+                case 1:
+                    lstItems.AddRange(new List<ShoppingCartItem>
+                    {
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "Book", Price = 12.49m, Type = Product.ProductType.Book, IsImport = false },
+                            Quantity = 1
+                        },
 
-                new ShoppingCartItem 
-                { 
-                    Item = new Product { Name = "Book", Price = 12.49m, Type = Product.ProductType.Book, IsImport = false }, 
-                    Quantity = 1 
-                },
-                new ShoppingCartItem {
-                    Item = new Product { Name = "music CD", Price = 14.99m, Type = Product.ProductType.Other, IsImport = false }, 
-                    Quantity = 1 
-                },
-                new ShoppingCartItem { 
-                    Item = new Product { Name = "chocolate bar", Price = 0.85m, Type = Product.ProductType.Food, IsImport = false }, 
-                    Quantity = 1 
-                },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "music CD", Price = 14.99m, Type = Product.ProductType.Other, IsImport = false },
+                            Quantity = 1
+                        },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "chocolate bar", Price = 0.85m, Type = Product.ProductType.Food, IsImport = false },
+                            Quantity = 1
+                        },
+                    });
+                    break;
 
-                //input 2
-                new ShoppingCartItem
-                {
-                    Item = new Product { Name = "imported box of chocolates", Price = 10.0m, Type = Product.ProductType.Food, IsImport = true },
-                    Quantity = 1
-                },
-                new ShoppingCartItem {
-                    Item = new Product { Name = "imported bottle of perfume", Price = 47.5m, Type = Product.ProductType.Other, IsImport = true },
-                    Quantity = 1
-                },
-                
-                //input 3
-                new ShoppingCartItem
-                {
-                    Item = new Product { Name = "imported bottle of perfume", Price = 27.99m, Type = Product.ProductType.Other, IsImport = true },
-                    Quantity = 1
-                },
-                new ShoppingCartItem 
-                {
-                    Item = new Product { Name = "bottle of perfume", Price = 18.99m, Type = Product.ProductType.Other, IsImport = false },
-                    Quantity = 1
-                },
-                new ShoppingCartItem 
-                {
-                    Item = new Product { Name = "packet of headache pills", Price = 9.75m, Type = Product.ProductType.Medical, IsImport = false },
-                    Quantity = 1
-                },
-                new ShoppingCartItem {
-                    Item = new Product { Name = "box of imported chocolates", Price = 11.25m, Type = Product.ProductType.Food, IsImport = true },
-                    Quantity = 1
-                },
+                case 2:
+                    lstItems.AddRange(new List<ShoppingCartItem>
+                    {
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "imported box of chocolates", Price = 10.0m, Type = Product.ProductType.Food, IsImport = true },
+                            Quantity = 1
+                        },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "imported bottle of perfume", Price = 47.5m, Type = Product.ProductType.Other, IsImport = true },
+                            Quantity = 1
+                        },
+                    });
+                    break;
+                case 3:
+                    lstItems.AddRange(new List<ShoppingCartItem>
+                    {
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "imported bottle of perfume", Price = 27.99m, Type = Product.ProductType.Other, IsImport = true },
+                            Quantity = 1
+                        },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "bottle of perfume", Price = 18.99m, Type = Product.ProductType.Other, IsImport = false },
+                            Quantity = 1
+                        },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "packet of headache pills", Price = 9.75m, Type = Product.ProductType.Medical, IsImport = false },
+                            Quantity = 1
+                        },
+                        new ShoppingCartItem
+                        {
+                            Item = new Product { Name = "box of imported chocolates", Price = 11.25m, Type = Product.ProductType.Food, IsImport = true },
+                            Quantity = 1
+                        },
+                    });
+                    break;
+                default:
+                    break;
+            }
 
-            };
-        }
+            return lstItems;
+        }   
             
     }
 }

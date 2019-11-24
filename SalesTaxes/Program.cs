a884ad1b1a5cbfd3a5cd3e60229ad32ebc45ea05
Alexandre Konioukhov <alexan1@gmail.com>
2019-11-23 19:02:38 -0500

    Util cllass added

--- SalesTaxes/Program.cs
+++ SalesTaxes/Program.cs
@@ -13,99 +13,11 @@ namespace SalesTaxes
 
             for (int list = 1; list < 4; list++)
             {
-                IEnumerable<ShoppingCartItem> itemList = GetItemsList(list);
-
-                var salestaxes = 0.00m;
-                var totalprice = 0.00m;
-
-                foreach (var item in itemList)
-                {
-                    salestaxes += item.Taxes * item.Quantity;
-                    totalprice += item.Item.Price * item.Quantity;
-                    Console.WriteLine("{0} {1} : {2}", item.Quantity, item.Item.Name, (item.Item.Price + item.Taxes) * item.Quantity);
-                }
-                totalprice += salestaxes;
-                Console.WriteLine("Sales Taxes : {0} ", salestaxes);
-                Console.WriteLine("Total : {0}", totalprice);
-                Console.ReadLine();
+                IEnumerable<ShoppingCartItem> itemList = Util.GetItemsList(list);
+                Util.PrintRecept(itemList);                
             }
+            Console.ReadLine();
         }
-
-        private static IEnumerable<ShoppingCartItem> GetItemsList(int listnum)
-        {
-
-            var lstItems = new List<ShoppingCartItem>();
-
-            switch (listnum)
-            {
-                case 1:
-                    lstItems.AddRange(new List<ShoppingCartItem>
-                    {
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "Book", Price = 12.49m, Type = Product.ProductType.Book, IsImport = false },
-                            Quantity = 1
-                        },
-
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "music CD", Price = 14.99m, Type = Product.ProductType.Other, IsImport = false },
-                            Quantity = 1
-                        },
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "chocolate bar", Price = 0.85m, Type = Product.ProductType.Food, IsImport = false },
-                            Quantity = 1
-                        },
-                    });
-                    break;
-
-                case 2:
-                    lstItems.AddRange(new List<ShoppingCartItem>
-                    {
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "imported box of chocolates", Price = 10.0m, Type = Product.ProductType.Food, IsImport = true },
-                            Quantity = 1
-                        },
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "imported bottle of perfume", Price = 47.5m, Type = Product.ProductType.Other, IsImport = true },
-                            Quantity = 1
-                        },
-                    });
-                    break;
-                case 3:
-                    lstItems.AddRange(new List<ShoppingCartItem>
-                    {
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "imported bottle of perfume", Price = 27.99m, Type = Product.ProductType.Other, IsImport = true },
-                            Quantity = 1
-                        },
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "bottle of perfume", Price = 18.99m, Type = Product.ProductType.Other, IsImport = false },
-                            Quantity = 1
-                        },
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "packet of headache pills", Price = 9.75m, Type = Product.ProductType.Medical, IsImport = false },
-                            Quantity = 1
-                        },
-                        new ShoppingCartItem
-                        {
-                            Item = new Product { Name = "box of imported chocolates", Price = 11.25m, Type = Product.ProductType.Food, IsImport = true },
-                            Quantity = 1
-                        },
-                    });
-                    break;
-                default:
-                    break;
-            }
-
-            return lstItems;
-        }   
             
     }
 }
--- SalesTaxesTests/SalesTaxesTests.cs
+++ SalesTaxesTests/SalesTaxesTests.cs
@@ -4,92 +4,99 @@ namespace SalesTaxes
 {
     [TestClass]
     public class SalesTaxesTests
-    {        
-            [TestMethod]
-            public void TestProduct1()
+    {
+        [TestMethod]
+        public void TestProduct1()
+        {
+            var product1 = new Product
             {
-                var product1 = new Product
-                {
-                    Name = "Book",
-                    Price = 12.49m,
-                    IsImport = false,
-                    Type = Product.ProductType.Book
-                };
-                var item1 = new ShoppingCartItem
-                {
-                    Item = product1,
-                    Quantity = 1
-                };
-
-                var actual = item1.Taxes;
-                var expected = 0.00m;
-                Assert.AreEqual(expected, actual, "Should be zero taxes");
-            }
-
-            [TestMethod]
-            public void TestProduct2()
+                Name = "Book",
+                Price = 12.49m,
+                IsImport = false,
+                Type = Product.ProductType.Book
+            };
+            var item1 = new ShoppingCartItem
             {
-                var product1 = new Product
-                {
-                    Name = "music CD",
-                    Price = 14.99m,
-                    IsImport = false,
-                    Type = Product.ProductType.Other
-                };
+                Item = product1,
+                Quantity = 1
+            };
 
-                var item1 = new ShoppingCartItem
-                {
-                    Item = product1,
-                    Quantity = 1
-                };
+            var actual = item1.Taxes;
+            var expected = 0.00m;
+            Assert.AreEqual(expected, actual, "Should be zero taxes");
+        }
 
-                var actual = item1.Taxes;
-                var expected = 1.5m;
-                Assert.AreEqual(expected, actual, "Wrong taxes");
-            }
-
-            [TestMethod]
-            public void TestProduct3()
+        [TestMethod]
+        public void TestProduct2()
+        {
+            var product1 = new Product
             {
-                var product1 = new Product
-                {
-                    Name = "imported box of chocolates",
-                    Price = 10.00m,
-                    IsImport = true,
-                    Type = Product.ProductType.Food
-                };
+                Name = "music CD",
+                Price = 14.99m,
+                IsImport = false,
+                Type = Product.ProductType.Other
+            };
 
-                var item1 = new ShoppingCartItem
-                {
-                    Item = product1,
-                    Quantity = 1
-                };
-
-                var actual = item1.Taxes;
-                var expected = 0.50m;
-                Assert.AreEqual(expected, actual, "Wrong taxes");
-            }
-
-            [TestMethod]
-            public void TestProduct4()
+            var item1 = new ShoppingCartItem
             {
-                var product1 = new Product
-                {
-                    Name = "imported bottle of perfume",
-                    Price = 47.50m,
-                    IsImport = true,
-                    Type = Product.ProductType.Other
-                };
+                Item = product1,
+                Quantity = 1
+            };
 
-                var item1 = new ShoppingCartItem
-                {
-                    Item = product1,
-                    Quantity = 1
-                };
+            var actual = item1.Taxes;
+            var expected = 1.5m;
+            Assert.AreEqual(expected, actual, "Wrong taxes");
+        }
 
-                var actual = item1.Taxes;
-                var expected = 7.15m;
-                Assert.AreEqual(expected, actual, "Wrong taxes");
-            }        
+        [TestMethod]
+        public void TestProduct3()
+        {
+            var product1 = new Product
+            {
+                Name = "imported box of chocolates",
+                Price = 10.00m,
+                IsImport = true,
+                Type = Product.ProductType.Food
+            };
+
+            var item1 = new ShoppingCartItem
+            {
+                Item = product1,
+                Quantity = 1
+            };
+
+            var actual = item1.Taxes;
+            var expected = 0.50m;
+            Assert.AreEqual(expected, actual, "Wrong taxes");
+        }
+
+        [TestMethod]
+        public void TestProduct4()
+        {
+            var product1 = new Product
+            {
+                Name = "imported bottle of perfume",
+                Price = 47.50m,
+                IsImport = true,
+                Type = Product.ProductType.Other
+            };
+
+            var item1 = new ShoppingCartItem
+            {
+                Item = product1,
+                Quantity = 1
+            };
+
+            var actual = item1.Taxes;
+            var expected = 7.15m;
+            Assert.AreEqual(expected, actual, "Wrong taxes");
+        }
+
+        [TestMethod]
+        public void TestCart1()
+        {
+
+
+        }
     }
 }
